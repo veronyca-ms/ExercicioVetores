165d399efdcc8eb1e620d0aba3ed3b820b8aea88
veronyca-ms <veronicasantos7262@gmail.com>
2025-03-21 00:04:35 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioVetores/Estudante.cs
@@ -0,0 +1,18 @@
+﻿namespace ExercicioVetores
+{
+    class Estudante
+    {
+        public string Nome { get; set; } 
+        public string Email { get; set; }
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
---
+++ ExercicioVetores/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ExercicioVetores
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.WriteLine("Quantos quartos deseja alugar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine($"Aluguel de Quartos {i}");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+            }
+            Console.WriteLine("Quarto Ocupado");
+            for (int i = 0; i < n; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
